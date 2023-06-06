ede4e85d3cab796f97894f467dd5f86bcbda0afe
CynthiaVM <80722354+CynthiaVM@users.noreply.github.com>
2023-06-06 00:32:16 -0300

    Create InterfacesCliente.ts

---
+++ src/Clientes/InterfacesCliente.ts
@@ -0,0 +1,14 @@
+export interface iclientes{
+    
+    id?:string;
+    nombre: string;
+    apellido: string;
+    telefono: number;
+    email:string;
+    empresa: string;
+    cuit :  number;
+    direccion : string;
+    localidad : string;
+    pais : string;
+	       
+ };
