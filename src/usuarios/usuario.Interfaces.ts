fbea70ef2a367e5dd6ba9adf3e7553c6ad59ade6
CynthiaVM <80722354+CynthiaVM@users.noreply.github.com>
2023-06-11 21:48:06 -0300

    Create usuario.Interfaces.ts

---
+++ src/usuarios/usuario.Interfaces.ts
@@ -0,0 +1,13 @@
+
+export interface iUsuario {
+  nombre: string;
+  apellido: string;
+  telefono: string;
+  email: string;
+  empresa: string;
+  cuit: string;
+  direccion: string;
+  localidad: string;
+  pais: string;
+  // aca definir las propiedades de la interfaz 
+}
